919ea928000b914055179fcf0bd048a4c902c31d
nickhrm <nickhrm2308@gmail.com>
2024-08-18 19:07:54 +0200

    changed to hugo

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['strapi.nickhrm.de'],
-    },
-};
-
-export default nextConfig;
--- src/app/projects/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton"
-
-
-export default function Page() {
-    return <Skeleton className="w-full h-full rounded-full" />
-
-}
