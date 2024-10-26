ee2b925d4c50278c6f78db600620cd90c20bab5c
nickhrm <nickhrm2308@gmail.com>
2024-10-26 11:18:56 +0200

    Update site configuration, adjust layout spacing, and remove unused favicon files

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://nickhrm.de',
+  
+  
 });
