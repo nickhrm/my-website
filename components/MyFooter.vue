8ab2db3646f984b977c54df11c5c579ea93df7f2
nickhrm <nickhrm2308@gmail.com>
2025-11-08 12:23:13 +0100

    removed cycling gif

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="w-full flex flex-col items-center mt-16">
-    <BicycleLogo></BicycleLogo>
+    <!-- <BicycleLogo></BicycleLogo> -->
     <div class="flex flex-col md:flex-row justify-center items-center pb-10 pt-3 gap-4">
       <UButton to="/imprint" variant="ghost" color="black" class="hover:underline text-neutral-500">Impressum</UButton>
-      <UButton
+      <!-- <UButton
         to="https://www.flaticon.com/free-animated-icons/bicycle"
         variant="ghost"
         color="black"
         class="hover:underline text-neutral-500"
         >Bicycle animated icons created by Freepik - Flaticon</UButton
-      >
+      > -->
     </div>
   </div>
 </template>
