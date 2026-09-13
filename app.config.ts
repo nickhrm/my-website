a2795050838774e0d11f06e28ff846860255ca04
nickhrm <nickhrm2308@gmail.com>
2026-09-13 19:25:03 +0200

    c

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+    ui: {
+      primary: 'green',
+
+      gray: 'neutral',
+
+      container: {
+        constrained: 'max-w-3xl',
+      },
+
+      
+    }
+  })
